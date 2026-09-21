b07d75d027cb2b1a6ddf5138bf5fa96c9ac53550
Gordo <gord.turner@gmail.com>
2026-09-21 17:11:50 -0400

    Add neon reflection background effect inspired by cyberpunk street scene

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import { ArrowDown, ArrowUpRight, BriefcaseBusiness, CheckCircle2, Github, Linkedin, Mail } from "lucide-react";
 import { experience, projects } from "./data";
 import SiteChrome from "./components/SiteChrome";
+import NeonReflection from "./components/NeonReflection";
 import SiteHeader, { SITE_NAV_ITEMS } from "./components/SiteHeader";
 import VoightKampff from "./components/VoightKampff";
 
@@ -13,6 +14,7 @@ const fadeUp = {
 function App() {
   return (
     <div className="site-shell">
+      <NeonReflection />
       <SiteChrome />
       <SiteHeader navItems={SITE_NAV_ITEMS} />
 
